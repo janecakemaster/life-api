5a24e0f9198a8c5afca7ca87e0a845c163da4e79
Jane Kim <jane@janekim.me>
2016-06-20 16:51:03 -0400

    update to gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,7 +5,7 @@ import eslint from 'gulp-eslint'
 import concat from 'gulp-concat'
 import babel from 'gulp-babel'
 import uglify from 'gulp-uglify'
-import clean from 'gulp-clean'
+import del from 'del'
 import livereload from 'gulp-livereload'
 
 const dirs = {
@@ -17,43 +17,34 @@ const dirs = {
   clean: ['public/assets/**/*.js', 'public/assets/**/*.css', 'public/assets/**/*.map']
 }
 
-gulp.task('clean', () => {
-  return gulp.src(dirs.clean, {read: false})
-    .pipe(clean())
-})
-
-gulp.task('styles', () => {
-  return gulp.src(`${dirs.styles}app.scss`)
+export const clean = () => del(dirs.clean)
+export const styles = () =>
+  gulp.src(`${dirs.styles}app.scss`)
     .pipe(sourcemaps.init())
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(dirs.dest))
-})
 
-gulp.task('scripts', () => {
-  return gulp.src(`${dirs.scripts}**/*.js`)
+export const scripts = () =>
+  gulp.src(`${dirs.scripts}**/*.js`)
     .pipe(eslint())
     .pipe(babel({
       presets: ['es2015'],
     }))
     .pipe(gulp.dest(dirs.dest))
-})
 
-gulp.task('libs', () => {
-  return gulp.src(`${dirs.libs}**/*.js`)
+export const libs = () =>
+  gulp.src(`${dirs.libs}**/*.js`)
     .pipe(gulp.dest(dirs.dest))
-})
 
-gulp.task('reload', () => {
-  livereload.reload()
-})
+export const reload = () => livereload.reload()
 
-gulp.task('watch', () => {
+export const watch = () => {
   livereload.listen()
-  gulp.watch(`${dirs.styles}**/*.scss`, ['styles'])
-  gulp.watch(`${dirs.scripts}**/*.js`, ['scripts'])
-  gulp.watch('public/**/*', ['reload'])
-})
+  gulp.watch(`${dirs.styles}**/*.scss`, styles)
+  gulp.watch(`${dirs.scripts}**/*.js`, scripts)
+  gulp.watch('public/**/*', reload)
+}
 
-gulp.task('build', ['styles', 'libs', 'scripts'])
-gulp.task('default', ['watch', 'build'])
+export const build = gulp.parallel(styles, libs, scripts)
+export default gulp.parallel(watch, build)
